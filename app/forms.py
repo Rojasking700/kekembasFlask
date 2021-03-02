34539916418604b6ff0314586dd4087c64bfc721
gabriel rojas <grojaspiano@gmail.com>
2021-03-02 17:26:37 -0600

    tuesday lesson

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Email
+
+
+class UserInfoForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm PassWord', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template,request
+from app.forms import UserInfoForm, PostForm
 
 @app.route('/')
 @app.route('/index')
@@ -8,26 +9,30 @@ def hello_world():
     context = {
         'title' : 'Kekembas Blog | HOME',
         'customer_name': 'brian',
-        'customer_username': 'bstanton',
-        'items':{
-            1: 'icream',
-            2: 'bread'
-        },
-        'followers' : [
-            {
-                'username' : 'sdavvit',
-                'created_at' :'2021-02-28'
-            },
-            {
-                'username' : 'jacrter',
-                'created_at' :'2020-09-08'
-            }
-        ]
+        'customer_username': 'bstanton'
     }
+        
     return  render_template('index.html', **context)
 
  
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     title = 'Kekembas blog | REGISTER'
-    return render_template('register.html', title=title)
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate():
+        # username = form.username.data
+        # email = form.email.data
+        # password = form.password.data
+        print("hello")
+
+    return render_template('register.html', title=title,form=form)
+
+@app.route('/createpost', methods=['GET','POST'])
+def createpost():
+    title = " Kekembas blog | CREATE POST"
+    post = PostForm()
+    if request.method == "POST" and post.validate():
+        post_title = post.title.data
+        content = post.content.data
+        print(post_title, content)
+    return render_template('create_post.html', post=post, title=title)
