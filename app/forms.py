d91f05e9ab15fd2bc0de12595153f1109988037f
gabriel rojas <grojaspiano@gmail.com>
2021-03-03 18:13:14 -0600

    wednesday commit

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, Email
 
 
@@ -7,10 +7,16 @@ class UserInfoForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_pass = PasswordField('Confirm PassWord', validators=[DataRequired(), EqualTo('password')])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField()
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField()
+
+class LoginForm(FlaskForm):
+    username = StringField('username',validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField()
